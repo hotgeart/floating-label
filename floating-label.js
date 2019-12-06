c3f177bc6a07513e78974f014b340cfa6f61b8c7
thomasmester <thomas@blue4you.be>
2019-12-06 14:38:48 +0100

    update

--- floating-label.js
+++ floating-label.js
@@ -7,29 +7,116 @@
 
 (function($) {
   $.fn.floatingLabel = function(options) {
-    if (this.length) {
+    this.each(function() {
       // Default variable
-      var input = "#" + this[0].id;
+      var input = "#" + this.id;
       input = $(input);
       var container = $(input).parent();
-      var label = $(input).parent().children('label');
-      
-      container.addClass('floating-label');
-      container.css('position', 'relative');
-      label.css({
-        'position': 'absolute',
-        'top': 0,
-        'left': input.css('padding-left'),
-        'font-size': input.css('font-size'),
-        'line-height': input.css('height'),
-        'font-style': 'italic',
-        'margin': 0,
-        'padding': 0
-      });
+      var label = $(input)
+        .parent()
+        .children("label");
+      var notFirstTime = false; // to avoid to play the animation the first time
 
-      input.focusin(function() {
-        
+      // Defaults settings
+      var defaults = {
+        transition: true,
+        transitionProperties: "all",
+        transitionDuration: ".2s",
+        placeHolderColor: false
+      };
+
+      var settings = $.extend({}, defaults, options);
+
+      // Setup
+      container.addClass("floating-label");
+      container.css("position", "relative");
+      label.addClass("floating");
+      style();
+
+      // Style
+      function style(forceFloat) {
+        // Floating
+        if (
+          input.hasClass("not-empty") ||
+          input.val().length > 0 ||
+          forceFloat
+        ) {
+          label.removeClass("not");
+          label.css({
+            position: "absolute",
+            top: 0,
+            left: input.css("padding-left"),
+            color: input.css("color"),
+            fontSize: "10px",
+            lineHeight: "1",
+            fontStyle: "normal",
+            textTransform: "uppercase",
+            backgroundColor: input.css("background-color"),
+            transform: "translateY(-50%)",
+            margin: "0 0 0 -3px",
+            padding: "1px 3px"
+          });
+
+          if (settings.transition && notFirstTime) {
+            label.css({
+              "transition-property": settings.transitionProperties,
+              "transition-duration": settings.transitionDuration
+            });
+          }
+          // NOT floating
+        } else {
+          label.addClass("not");
+          label.css({
+            position: "absolute",
+            top: input.css("padding-top"),
+            left: input.css("padding-left"),
+            fontSize: input.css("font-size"),
+            lineHeight: "",
+            fontStyle: "italic",
+            textTransform: "none",
+            backgroundColor: "transparent",
+            transform: "none",
+            margin: 0,
+            padding: 0
+          });
+
+          if (settings.transition && notFirstTime) {
+            label.css({
+              "transition-property": settings.transitionProperties,
+              "transition-duration": settings.transitionDuration
+            });
+          }
+
+          if (settings.placeHolderColor !== false) {
+            label.css("color", settings.placeHolderColor);
+          }
+        }
+      }
+
+      input
+        .focusin(function() {
+          notFirstTime = true;
+          style(true);
+        })
+        .focusout(function() {
+          notFirstTime = true;
+          style();
+        });
+
+      // Add/remove not-empty class if we're typing content
+      input.keyup(function() {
+        if (input.val().length === 0) {
+          input.removeClass("not-empty");
+        } else {
+          input.addClass("not-empty");
+        }
       });
-    }
+    });
+    // Add not-empty class if the input isn't empty
+    $("input[type=text]").each(function() {
+      if ($(this).val().length != 0) {
+        $(this).addClass("not-empty");
+      }
+    });
   };
 })(jQuery);
